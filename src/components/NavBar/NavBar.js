6af0c215864695c46e1f4d8322c9eccbaf460e87
kevin-wairi <kevinwairi@gmail.com>
2023-08-01 19:32:05 +0300

    updated Navbar design

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,14 @@
 import React from "react";
+import telephone from '../../assets/telephone.png'
+import email from '../../assets/email.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import twitter from '../../assets/twitter.png'
+import youtube from '../../assets/youtube.png'
+import cart from '../../assets/shopping-cart.png'
+import userIcon from '../../assets/user-icon.png'
 import { NavLink, useNavigate } from "react-router-dom";
+import './NavBar.css'
 
 function NavBar({user,logOut}){
     const navigate = useNavigate()
@@ -17,11 +26,39 @@ function NavBar({user,logOut}){
 
    
     return(
+        <>
+        <div className="row bg-black mx-auto">
+        <div class="col-md-4 col-sm-6 col-12 d-flex">
+            <div class="d-flex align-items-center col-md-6 col-sm-6 col-12 ">
+                <img src={telephone} class="img-fluid telephone logo-img" alt="phone number" />
+                <div><p className="pt-1 mb-0 text-white">(+254) 710 605 821</p></div>
+            </div>
+            <div class="d-flex align-items-center col-md-6 col-sm-6 col-12">
+                <img src={email} class="img-fluid telephone logo-img" alt="phone number" />
+                <p className=" mb-0 text-white">easymart@example.com</p>
+            </div>
+        </div>
+        <div className="col-md-3 col-sm-4 col-12 mx-auto">
+            { user ? (
+                    <p className="text-white "><small>Welcome {user[[0]].name}</small></p>
+            ) : (
+                <p className="text-white"><small>sign in for variety of products from our shop</small></p>
+            )}
+            
+        </div>
+            <div className="col-md-3 col-sm-4 col-12 d-flex">
+                <p className="text-white pt-2 mb-0 ">Follow us:</p>
+                    <img className="logo-img" src={facebook}  alt="" />
+                    <img className="logo-img" src={instagram} alt="" />
+                    <img className="logo-img" src={twitter}  alt="" />
+                    <img className="logo-img" src={youtube}  alt="" />
+            </div>
+        </div>
         <nav className="navbar navbar-expand-lg navbar-expand-md">
             <div className="container-fluid">
-            <NavLink className={"navbar-brand"}  exact to='/'>EASYMART </NavLink>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <NavLink className="navbar-brand fw-bold fs-3 p-0"  exact to='/'>EASYMART </NavLink>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="nav-item navbar-nav mx-auto">
@@ -33,18 +70,18 @@ function NavBar({user,logOut}){
                         
                         { user ? (
                         <>
-                            <NavLink  className={"nav-link"} to='/userProfile'>User Profile</NavLink>
+                            <NavLink  className={"nav-link"} to='/userProfile'><img src={userIcon} className="m-0 logo-img" alt="user-icon"/> User</NavLink>
                             <NavLink  className={"nav-link"} type="button" onClick={handleLogout}>Logout</NavLink>
-                            <NavLink  className={"nav-link"} to='/cart'>Cart</NavLink>
+                            <NavLink  className={"nav-link"} to='/cart'><img className="m-0 logo-img" src={cart} alt="shopping cart" /></NavLink>
                             
                             
                         </>
                         ) : (
                         <>
-                            <NavLink  className={"nav-link"} to='/adminProfile'>Admin Profile</NavLink>
+                            <NavLink  className={"nav-link"} to='/adminProfile'><img src={userIcon} className="m-0 logo-img" alt="user-icon"/> Admin </NavLink>
                             <NavLink  className={"nav-link"} to='/signup'>SignUp</NavLink>
                             <NavLink  className={"nav-link"} to='/login'>LogIn</NavLink>
-                            <NavLink  className={"nav-link"} to='/cart'>Cart</NavLink>
+                            <NavLink  className={"nav-link"} to='/cart'><img className="m-0 logo-img" src={cart} alt="shopping cart" /></NavLink>
                         </>
                         )}
                         {/* <NavLink  className={"nav-link"} to='/checkout'>Checkout</NavLink>  should checkout be on Navbar, i dont think it should be a component actually*/} 
@@ -52,6 +89,7 @@ function NavBar({user,logOut}){
             </div>
             </div>
         </nav>
+        </>
     )
 }
 export default NavBar
