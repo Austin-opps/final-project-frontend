d529971a24e5f6c10e3a5b18604c245127abd8ed
kevin-wairi <kevinwairi@gmail.com>
2023-08-09 20:29:17 +0300

    updated productscards disp

--- src/pages/ProductCard/ProductCard.js
+++ src/pages/ProductCard/ProductCard.js
@@ -25,16 +25,12 @@ const ProductCard = ({ product, navigate }) => {
       className="card product-card col-md-2 col-sm-3 col-4 flex-item m-2"
     >
       <div className="product-card-image text-center">
-      {product.galleryThumbnails && product.galleryThumbnails.length > 0 ? (
-  <img
-    className="img-fluid img"
-    src={isFirstImage ? product.galleryThumbnails[0].image : product.galleryThumbnails[1].image}
-    alt={isFirstImage ? "Image 1" : "Image 2"}
-  />
-) : (
-  <p>No images available.</p>
-)}
-
+        <img
+          className="img-fluid img "
+          src={isFirstImage ? product.galleryThumbnails[0].image : product.galleryThumbnails[1].image}
+          alt={isFirstImage ? "Image 1" : "Image 2"}
+        />
+      </div>
       <div className="card-body p-1 text-center">
               <p className="card-title m-0 text-md text-xs  text-sm text-success">
                 {product.name.substring(0, 30)}
@@ -43,7 +39,6 @@ const ProductCard = ({ product, navigate }) => {
               <p className="lead card-text m-0"><span className="ksh"><sup>Ksh.</sup></span>{product.price}</p>
               {/* <button className="btn col-11 shadow bg-success text-white">View Details</button> */}
       </div>
-    </div>
     </div>
   );
 };
